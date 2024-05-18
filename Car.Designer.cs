1701b359ab2f6934017b9e653465eef76edb876a
mohammedmostafa-1 <mohammedmostafaofficial68111@gmail.com>
2024-05-18 13:25:07 +0600

    Add files via upload

---
+++ Car.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace CarCrowd
+{
+    partial class Car
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Car));
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Homepagepanel1 = new Panel();
+            guna2GradientPanel1 = new Guna.UI2.WinForms.Guna2GradientPanel();
+            HomepageSearchguna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            Homepagelabel2 = new Label();
+            guna2CirclePictureBox1 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            Homepagelabel1 = new Label();
+            HomepageLoginButton1 = new Guna.UI2.WinForms.Guna2Button();
+            HomepagepictureBox1 = new PictureBox();
+            guna2ContextMenuStrip1 = new Guna.UI2.WinForms.Guna2ContextMenuStrip();
+            label1 = new Label();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            Homepagepanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)guna2CirclePictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)HomepagepictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // Homepagepanel1
+            // 
+            Homepagepanel1.BackColor = Color.Transparent;
+            Homepagepanel1.BackgroundImage = (Image)resources.GetObject("Homepagepanel1.BackgroundImage");
+            Homepagepanel1.Controls.Add(guna2GradientPanel1);
+            Homepagepanel1.Controls.Add(HomepageSearchguna2TextBox1);
+            Homepagepanel1.Controls.Add(Homepagelabel2);
+            Homepagepanel1.Controls.Add(guna2CirclePictureBox1);
+            Homepagepanel1.Controls.Add(Homepagelabel1);
+            Homepagepanel1.Controls.Add(HomepageLoginButton1);
+            Homepagepanel1.Controls.Add(HomepagepictureBox1);
+            Homepagepanel1.Location = new Point(-5, 1);
+            Homepagepanel1.Margin = new Padding(4, 3, 4, 3);
+            Homepagepanel1.Name = "Homepagepanel1";
+            Homepagepanel1.Size = new Size(1236, 307);
+            Homepagepanel1.TabIndex = 5;
+            // 
+            // guna2GradientPanel1
+            // 
+            guna2GradientPanel1.BackColor = Color.White;
+            guna2GradientPanel1.CustomizableEdges = customizableEdges1;
+            guna2GradientPanel1.Location = new Point(1047, 22);
+            guna2GradientPanel1.Margin = new Padding(4, 3, 4, 3);
+            guna2GradientPanel1.Name = "guna2GradientPanel1";
+            guna2GradientPanel1.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            guna2GradientPanel1.Size = new Size(4, 52);
+            guna2GradientPanel1.TabIndex = 6;
+            // 
+            // HomepageSearchguna2TextBox1
+            // 
+            HomepageSearchguna2TextBox1.BackgroundImageLayout = ImageLayout.Center;
+            HomepageSearchguna2TextBox1.BorderColor = Color.White;
+            HomepageSearchguna2TextBox1.BorderRadius = 50;
+            HomepageSearchguna2TextBox1.BorderStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+            HomepageSearchguna2TextBox1.BorderThickness = 50;
+            HomepageSearchguna2TextBox1.Cursor = Cursors.IBeam;
+            customizableEdges3.BottomLeft = false;
+            HomepageSearchguna2TextBox1.CustomizableEdges = customizableEdges3;
+            HomepageSearchguna2TextBox1.DefaultText = "Search model, manufacturer,  or mileage";
+            HomepageSearchguna2TextBox1.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            HomepageSearchguna2TextBox1.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            HomepageSearchguna2TextBox1.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            HomepageSearchguna2TextBox1.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            HomepageSearchguna2TextBox1.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            HomepageSearchguna2TextBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            HomepageSearchguna2TextBox1.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            HomepageSearchguna2TextBox1.IconRight = (Image)resources.GetObject("HomepageSearchguna2TextBox1.IconRight");
+            HomepageSearchguna2TextBox1.Location = new Point(423, 231);
+            HomepageSearchguna2TextBox1.Margin = new Padding(4, 3, 4, 3);
+            HomepageSearchguna2TextBox1.Name = "HomepageSearchguna2TextBox1";
+            HomepageSearchguna2TextBox1.PasswordChar = '\0';
+            HomepageSearchguna2TextBox1.PlaceholderText = "";
+            HomepageSearchguna2TextBox1.SelectedText = "";
+            HomepageSearchguna2TextBox1.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            HomepageSearchguna2TextBox1.Size = new Size(396, 42);
+            HomepageSearchguna2TextBox1.Style = Guna.UI2.WinForms.Enums.TextBoxStyle.Material;
+            HomepageSearchguna2TextBox1.TabIndex = 9;
+            HomepageSearchguna2TextBox1.UseWaitCursor = true;
+            HomepageSearchguna2TextBox1.WordWrap = false;
+            // 
+            // Homepagelabel2
+            // 
+            Homepagelabel2.AutoSize = true;
+            Homepagelabel2.BackColor = Color.Transparent;
+            Homepagelabel2.Font = new Font("Microsoft YaHei", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Homepagelabel2.ForeColor = Color.White;
+            Homepagelabel2.Location = new Point(423, 185);
+            Homepagelabel2.Margin = new Padding(4, 0, 4, 0);
+            Homepagelabel2.Name = "Homepagelabel2";
+            Homepagelabel2.Size = new Size(396, 31);
+            Homepagelabel2.TabIndex = 8;
+            Homepagelabel2.Text = "Car buying fashioned to your life!";
+            // 
+            // guna2CirclePictureBox1
+            // 
+            guna2CirclePictureBox1.BackColor = Color.Transparent;
+            guna2CirclePictureBox1.Image = (Image)resources.GetObject("guna2CirclePictureBox1.Image");
+            guna2CirclePictureBox1.ImageRotate = 0F;
+            guna2CirclePictureBox1.Location = new Point(1148, 22);
+            guna2CirclePictureBox1.Margin = new Padding(4, 3, 4, 3);
+            guna2CirclePictureBox1.Name = "guna2CirclePictureBox1";
+            guna2CirclePictureBox1.ShadowDecoration.CustomizableEdges = customizableEdges5;
+            guna2CirclePictureBox1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            guna2CirclePictureBox1.Size = new Size(48, 46);
+            guna2CirclePictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            guna2CirclePictureBox1.TabIndex = 6;
+            guna2CirclePictureBox1.TabStop = false;
+            guna2CirclePictureBox1.UseTransparentBackground = true;
+            // 
+            // Homepagelabel1
+            // 
+            Homepagelabel1.AutoSize = true;
+            Homepagelabel1.BackColor = Color.Transparent;
+            Homepagelabel1.Font = new Font("Microsoft YaHei", 27.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Homepagelabel1.ForeColor = Color.White;
+            Homepagelabel1.Location = new Point(423, 127);
+            Homepagelabel1.Margin = new Padding(4, 0, 4, 0);
+            Homepagelabel1.Name = "Homepagelabel1";
+            Homepagelabel1.Size = new Size(396, 50);
+            Homepagelabel1.TabIndex = 7;
+            Homepagelabel1.Text = "DRIVE YOU DREAM!";
+            // 
+            // HomepageLoginButton1
+            // 
+            HomepageLoginButton1.Animated = true;
+            HomepageLoginButton1.AutoRoundedCorners = true;
+            HomepageLoginButton1.BackColor = Color.Transparent;
+            HomepageLoginButton1.BorderColor = Color.Transparent;
+            HomepageLoginButton1.BorderRadius = 25;
+            HomepageLoginButton1.BorderStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+            HomepageLoginButton1.BorderThickness = 20;
+            HomepageLoginButton1.CustomizableEdges = customizableEdges6;
+            HomepageLoginButton1.DisabledState.BorderColor = Color.DarkGray;
+            HomepageLoginButton1.DisabledState.CustomBorderColor = Color.DarkGray;
+            HomepageLoginButton1.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            HomepageLoginButton1.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            HomepageLoginButton1.FillColor = Color.Transparent;
+            HomepageLoginButton1.Font = new Font("Cascadia Code", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            HomepageLoginButton1.ForeColor = Color.White;
+            HomepageLoginButton1.Location = new Point(1050, 20);
+            HomepageLoginButton1.Margin = new Padding(4, 3, 4, 3);
+            HomepageLoginButton1.Name = "HomepageLoginButton1";
+            HomepageLoginButton1.ShadowDecoration.CustomizableEdges = customizableEdges7;
+            HomepageLoginButton1.Size = new Size(166, 52);
+            HomepageLoginButton1.TabIndex = 6;
+            HomepageLoginButton1.Text = " Logout";
+            HomepageLoginButton1.TextAlign = HorizontalAlignment.Left;
+            HomepageLoginButton1.TextFormatNoPrefix = true;
+            // 
+            // HomepagepictureBox1
+            // 
+            HomepagepictureBox1.Image = (Image)resources.GetObject("HomepagepictureBox1.Image");
+            HomepagepictureBox1.Location = new Point(464, -18);
+            HomepagepictureBox1.Margin = new Padding(4, 3, 4, 3);
+            HomepagepictureBox1.Name = "HomepagepictureBox1";
+            HomepagepictureBox1.Size = new Size(298, 184);
+            HomepagepictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            HomepagepictureBox1.TabIndex = 0;
+            HomepagepictureBox1.TabStop = false;
+            // 
+            // guna2ContextMenuStrip1
+            // 
+            guna2ContextMenuStrip1.Name = "guna2ContextMenuStrip1";
+            guna2ContextMenuStrip1.RenderStyle.ArrowColor = Color.FromArgb(151, 143, 255);
+            guna2ContextMenuStrip1.RenderStyle.BorderColor = Color.Gainsboro;
+            guna2ContextMenuStrip1.RenderStyle.ColorTable = null;
+            guna2ContextMenuStrip1.RenderStyle.RoundedEdges = true;
+            guna2ContextMenuStrip1.RenderStyle.SelectionArrowColor = Color.White;
+            guna2ContextMenuStrip1.RenderStyle.SelectionBackColor = Color.FromArgb(100, 88, 255);
+            guna2ContextMenuStrip1.RenderStyle.SelectionForeColor = Color.White;
+            guna2ContextMenuStrip1.RenderStyle.SeparatorColor = Color.Gainsboro;
+            guna2ContextMenuStrip1.RenderStyle.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SystemDefault;
+            guna2ContextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Microsoft YaHei", 27.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Image = (Image)resources.GetObject("label1.Image");
+            label1.ImageAlign = ContentAlignment.TopLeft;
+            label1.Location = new Point(224, 324);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(839, 50);
+            label1.TabIndex = 10;
+            label1.Text = "SELECT YOUR BRAND, DEFINE YOUR DRIVE!";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackgroundImage = (Image)resources.GetObject("pictureBox2.BackgroundImage");
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(253, 403);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(230, 206);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 12;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackgroundImage = (Image)resources.GetObject("pictureBox3.BackgroundImage");
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(498, 403);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(230, 206);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 13;
+            pictureBox3.TabStop = false;
+            pictureBox3.Click += pictureBox3_Click;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackgroundImage = (Image)resources.GetObject("pictureBox4.BackgroundImage");
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(743, 403);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(230, 206);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 14;
+            pictureBox4.TabStop = false;
+            pictureBox4.Click += pictureBox4_Click;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.BackgroundImage = (Image)resources.GetObject("pictureBox5.BackgroundImage");
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(985, 403);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(230, 206);
+            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox5.TabIndex = 15;
+            pictureBox5.TabStop = false;
+            pictureBox5.Click += pictureBox5_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(12, 403);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(230, 206);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 17;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // Car
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1224, 749);
+            Controls.Add(pictureBox1);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(label1);
+            Controls.Add(Homepagepanel1);
+            Name = "Car";
+            Text = "CarCrowd: View Cars";
+            Homepagepanel1.ResumeLayout(false);
+            Homepagepanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)guna2CirclePictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)HomepagepictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel Homepagepanel1;
+        private Guna.UI2.WinForms.Guna2GradientPanel guna2GradientPanel1;
+        private PictureBox HomepagepictureBox1;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox guna2CirclePictureBox1;
+        private Guna.UI2.WinForms.Guna2Button HomepageLoginButton1;
+        private Guna.UI2.WinForms.Guna2TextBox HomepageSearchguna2TextBox1;
+        private Label Homepagelabel2;
+        private Label Homepagelabel1;
+        private Guna.UI2.WinForms.Guna2ContextMenuStrip guna2ContextMenuStrip1;
+        private Label label1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox1;
+    }
+}
